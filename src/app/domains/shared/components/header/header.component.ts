b3fbad358575743d7e56ecd81117d86e4c4df65e
Raycris Maldonado <rmaldonado@Raycriss-MacBook-Pro.local>
2024-07-10 09:19:22 -0400

    adding cart to header

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  hideSideMenu = signal(true);
 
+  toogleSideMenu() {
+    this.hideSideMenu.update((prevState) => !prevState);
+  }
 }
